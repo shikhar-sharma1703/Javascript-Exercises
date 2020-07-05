ee679add9790a94ca29f2729c043221e2b3bb648
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-07-05 20:59:07 +0530

    space-age

---
+++ space-age.js
@@ -0,0 +1,16 @@
+const earthseconds = 31557600;
+
+const orbital_period = {
+  'earth':1,
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
+}
+export const age = (planet,seconds) => {
+  const planetSeconds = orbital_period[planet] * earthseconds;
+  return parseFloat((seconds/planetSeconds).toFixed(2));
+};
